6ac0e0ca982191bf61020fc301cf1084c70dbf60
waelepi <waeldhahri0@gmail.com>
2024-02-20 14:17:39 +0100

    1 st commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+
+  templateUrl: './servers.component.html',
+
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent implements OnInit{
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
